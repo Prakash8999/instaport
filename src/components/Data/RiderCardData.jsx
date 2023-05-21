25e00e686c5ae0a1b965a5aab9518111d69b8f0a
nitish0104 <nitishdalvi1@gmail.com>
2023-05-21 12:12:07 +0530

    Rider Change

---
+++ src/components/Data/RiderCardData.jsx
@@ -0,0 +1,19 @@
+const ridercardData = [
+  {
+    RiderName: "Shradha Magdum",
+    RiderAge: "21",
+    VechileNumber: "MH 17 6852",
+  },
+  {
+    RiderName: "Nitish ",
+    RiderAge: "21",
+    VechileNumber: "MH 17 6852",
+  },
+  {
+    RiderName: "More",
+    RiderAge: "21",
+    VechileNumber: "MH 17 6852",
+  },
+];
+
+export default ridercardData;
--- src/pages/ApproveARider.jsx
+++ src/pages/ApproveARider.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import Layout from "../components/Layout";
 import SideNav from "../components/SideNav";
 import Layout2 from "../components/Layout2";
-import RiderCard from "../components/RiderCard";
 import { NavLink } from "react-router-dom";
 import Search from "../components/Search";
+import Avtar from "../images/Avtar.png";
+
+import ridercardData from "../components/Data/RiderCardData";
 const ApproveARider = () => {
   return (
     <div>
@@ -34,31 +36,42 @@ const ApproveARider = () => {
         <Layout2>
           <div className="flex items-center justify-center my-8">
             <div className="grid grid-cols-3 gap-y-16 gap-x-16">
-              <RiderCard
-                RiderName={"Nitish mangesh Dalvi"}
-                RiderAge={"21"}
-                VechileNumber={"MH04 5678"}
-              ></RiderCard>
-              <RiderCard
-                RiderName={"om"}
-                RiderAge={"21"}
-                VechileNumber={"MH04 5678"}
-              ></RiderCard>
-              <RiderCard
-                RiderName={"Nitish Dalvi"}
-                RiderAge={"21"}
-                VechileNumber={"MH04 5678"}
-              ></RiderCard>
-              <RiderCard
-                RiderName={"Nitish Dalvi"}
-                RiderAge={"21"}
-                VechileNumber={"MH04 5678"}
-              ></RiderCard>
-              <RiderCard
-                RiderName={"Nitish Dalvi"}
-                RiderAge={"21"}
-                VechileNumber={"MH04 5678"}
-              ></RiderCard>
+              {ridercardData.map((data, index) => {
+                return (
+                  <div className="relative flex flex-col w-full h-full gap-5 bg-slate-100 rounded-xl   border-2 border-gray-400 hover:shadow-lg">
+                    <div
+                      className=" flex flex-col w-[inherit] h-[30vh] gap-y-11 items-center justify-center p-[2vw]"
+                      key={index}
+                    >
+                      <div className="flex  items-start  gap-8">
+                        <div>
+                          <img src={Avtar} alt="" />
+                        </div>
+                        <div className="flex flex-col">
+                          <div className="font-semibold">{data?.RiderName}</div>
+                          <div className="flex gap-3">
+                            <div className="text-sm text-slate-400">
+                              Age:{data?.RiderAge}
+                            </div>
+                            <span>|</span>
+                            <div className="text-sm text-slate-400">
+                              {data?.VechileNumber}
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="flex  items-center gap-[1vw]">
+                        <button className="border-2 bg-[#FFFDE6] text-black px-6 py-2 rounded-xl  border-green-400 hover:border-green-500 hover:border-dashed hover:bg-green-100  hover:shadow-lg ">
+                          Confirm
+                        </button>
+                        <button className="border-2 bg-[#FFFDE6] text-black px-8 py-2 rounded-xl   border-red-600 hover:border-red-500 hover:border-dashed hover:bg-red-100  hover:shadow-lg">
+                          Delete
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
             </div>
           </div>
         </Layout2>
