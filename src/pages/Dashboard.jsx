bd3a375192681b1113bc84b8a8f81fbbaa808d6f
Omjavir <javirom07042003@gmail.com>
2024-01-27 09:59:15 +0530

    Reformat: text color

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,70 +1,87 @@
 import React, { useState } from "react";
 import SideNav from "../components/SideNav";
 import Layout from "../components/Layout";
-import Search from "../components/Search";
 import StatsCard from "../components/StatsCard";
 import { Link } from "react-router-dom";
 import { ContextAuth } from "../context/Context";
 import ChartModal from "../components/Modal/ChartModal";
-import thead from "../components/Data/TableheadingOrder";
 import moment from "moment";
 
 const Dashboard = () => {
-  const { orders } = ContextAuth()
-  const [modal, setModal] = useState({ show: false })
-
+  const { orders } = ContextAuth();
+  const [modal, setModal] = useState({ show: false });
 
   return (
     <div>
-
-      {
-        modal.show && (
-          <ChartModal orders={orders} dataModal={modal.show}
-            setModal={setModal}
-          />
-        )
-      }
+      {modal.show && (
+        <ChartModal
+          orders={orders}
+          dataModal={modal.show}
+          setModal={setModal}
+        />
+      )}
       <Layout>
         <div className="flex pl-5 justify-between items-start">
-          <SideNav className={'w-[20vw]'} />
+          <SideNav className={"w-[20vw]"} />
 
           <div className="h-fit w-[79vw] px-5 pt-4">
             <div className=" justify-around  items-center hidden ">
-
-              <Link to={'/orders?query=new'} className="h-16 w-64 flex justify-center items-center  bg-white  rounded-xl shadow-lg border duration-200 hover:shadow-xl cursor-pointer ">
-                {orders?.filter(data => data?.status == 'new').length} New Orders in last 24 Hours
+              <Link
+                to={"/orders?query=new"}
+                className="h-16 w-64 flex justify-center items-center  bg-white  rounded-xl shadow-lg border duration-200 hover:shadow-xl cursor-pointer "
+              >
+                {orders?.filter((data) => data?.status === "new").length} New
+                Orders in last 24 Hours
               </Link>
-              <Link to={'/orders?query=new'} className="h-16 w-64 justify-center flex items-center  bg-white  rounded-xl shadow-lg border duration-200 hover:shadow-xl cursor-pointer ">
+              <Link
+                to={"/orders?query=new"}
+                className="h-16 w-64 justify-center flex items-center  bg-white  rounded-xl shadow-lg border duration-200 hover:shadow-xl cursor-pointer "
+              >
                 0 New Orders in last 24 Hours
               </Link>
-              <button onClick={() => {
-                setModal({ show: true })
-              }}
-                className="h-16 w-64 justify-center  flex items-center bg-white rounded-xl shadow-lg border duration-200 hover:shadow-xl cursor-pointer ">
+              <button
+                onClick={() => {
+                  setModal({ show: true });
+                }}
+                className="h-16 w-64 justify-center  flex items-center bg-white rounded-xl shadow-lg border duration-200 hover:shadow-xl cursor-pointer "
+              >
                 Chart
               </button>
-
-
-
             </div>
             <div className="gap-x-6 grid grid-cols-4 mt-14 justify-items-center">
               {/* <div className="flex items-center justify-around mt-14"> */}
               {/* <Link to="/orders?query=processing"> */}
-              <StatsCard Number={orders?.length > 0 ? orders?.length : 0} Value={"Total Order"}></StatsCard>
+              <StatsCard
+                Number={orders?.length > 0 ? orders?.length : 0}
+                Value={"Total Order"}
+              ></StatsCard>
               {/* </Link> */}
               <Link className="w-full" to="/orders?query=new">
-                <StatsCard Number={orders?.filter(data => data?.status == 'new').length} Value={"New"}></StatsCard>
+                <StatsCard
+                  Number={
+                    orders?.filter((data) => data?.status === "new").length
+                  }
+                  Value={"New"}
+                ></StatsCard>
               </Link>
               <Link className="w-full" to="/orders?query=processing">
-                <StatsCard Number={orders?.filter(data => data?.status == 'processing').length} Value={"Processing"}></StatsCard>
+                <StatsCard
+                  Number={
+                    orders?.filter((data) => data?.status === "processing")
+                      .length
+                  }
+                  Value={"Processing"}
+                ></StatsCard>
               </Link>
               <Link className="w-full" to="/orders?query=delivered">
                 <StatsCard
-                  Number={orders?.filter(data => data?.status == 'delivered').length}
+                  Number={
+                    orders?.filter((data) => data?.status === "delivered")
+                      .length
+                  }
                   Value={" Delivered"}
                 ></StatsCard>
               </Link>
-
             </div>
             <div className=" flex justify-center items-start w-[76vw]  h-[80vh] overflow-y-scroll mt-6 ">
               <div className="w-full">
@@ -76,27 +93,13 @@ const Dashboard = () => {
           {head.tablehead}
         </th>
       ))} */}
-                      <th>
-                        Order ID
-                      </th>
-                      <th>
-                        Customer Name
-                      </th>
-                      <th>
-                        Customer No
-                      </th>
-                      <th>
-                        Date
-                      </th>
-                      <th>
-                        Time
-                      </th>
-                      <th>
-                        Payment Type
-                      </th>
-                      <th>
-                        Status
-                      </th>
+                      <th>Order ID</th>
+                      <th>Customer Name</th>
+                      <th>Customer No</th>
+                      <th>Date</th>
+                      <th>Time</th>
+                      <th>Payment Type</th>
+                      <th>Status</th>
                     </tr>
                   </thead>
 
@@ -128,10 +131,12 @@ const Dashboard = () => {
                             //   setmodal({ show: true, datamodal: data });
                             // }}
                             onClick={() => {
-                              window.open(`order/${data?._id}`, '_blank', 'noopener, noreferrer');
-
+                              window.open(
+                                `order/${data?._id}`,
+                                "_blank",
+                                "noopener, noreferrer"
+                              );
                             }}
-
                             target="_blank"
                             className="border-b-2 border-slate-100 bg-white odd:bg-gray-50 hover:scale-105 duration-300"
                           >
@@ -145,7 +150,6 @@ const Dashboard = () => {
                               {data?.phone_number}
                             </td>
 
-
                             <td className="cursor-pointer px-2 py-2  font-light">
                               {formattedDateString}
                             </td>
@@ -155,7 +159,19 @@ const Dashboard = () => {
                             <td className="cursor-pointer px-2 py-2  font-light">
                               {data?.payment_method}
                             </td>
-                            <td className={`capitalize cursor-pointer   font-semibold text-lg ${data?.status == 'delivered' ? 'text-green-500' : data?.status == 'processing' ? 'text-orange-500' : data?.status == 'new' ? 'text-yellow-500 rounded-lg ' : data?.status == 'cancelled' ? 'text-red-500' :'text-black'}`}>
+                            <td
+                              className={`capitalize cursor-pointer   font-semibold text-lg ${
+                                data?.status === "delivered"
+                                  ? "text-green-500"
+                                  : data?.status === "processing"
+                                    ? "text-orange-400"
+                                    : data?.status === "new"
+                                      ? "text-yellow-500 rounded-lg "
+                                      : data?.status === "cancelled"
+                                        ? "text-red-700"
+                                        : "text-black"
+                              }`}
+                            >
                               {data?.status}
                             </td>
                           </tr>
@@ -178,8 +194,6 @@ const Dashboard = () => {
               </div>
             </div>
           </div>
-
-
         </div>
       </Layout>
     </div>
