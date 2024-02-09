c6dfb6fd82f65f594ec8e4ab2458c5cc33858699
Prakash Jha <jhaprakash504@gmail.com>
2024-02-09 21:55:49 +0530

    Fix: Logic of the Downlaod Invoice Button

--- src/components/DownloadInvoice.jsx
+++ src/components/DownloadInvoice.jsx
@@ -4,6 +4,35 @@ import { jsPDF } from "jspdf";
 
 const DownloadInvoice = ({ rootElementId, downloadFileName }) => {
   const downloadPdfDocument = () => {
+    // let printContents = document.getElementById(rootElementId).innerHTML;
+    // let iframe = document.createElement('iframe');
+    // iframe.style.display = 'none'; // Hide the iframe
+    // document.body.appendChild(iframe);
+    // let doc = iframe.contentDocument || iframe.contentWindow.document;
+
+    // // Copy stylesheets from parent document to iframe document
+    // Array.from(document.styleSheets).forEach(styleSheet => {
+    //     if (styleSheet.href) { // External stylesheet
+    //         let link = document.createElement('link');
+    //         link.href = styleSheet.href;
+    //         link.rel = 'stylesheet';
+    //         doc.head.appendChild(link);
+    //     } else if (styleSheet.cssRules) { // Inline stylesheet
+    //         let style = document.createElement('style');
+    //         Array.from(styleSheet.cssRules).forEach(cssRule => {
+    //             style.appendChild(document.createTextNode(cssRule.cssText));
+    //         });
+    //         doc.head.appendChild(style);
+    //     }
+    // });
+
+    // // Set the contents and print
+    // doc.body.innerHTML = printContents;
+    // iframe.contentWindow.print();
+
+    // // Clean up: remove the iframe after printing
+    // document.body.removeChild(iframe);
+
     let printContents = document.getElementById(rootElementId).innerHTML;
     let iframe = document.createElement('iframe');
     iframe.style.display = 'none'; // Hide the iframe
@@ -16,6 +45,12 @@ const DownloadInvoice = ({ rootElementId, downloadFileName }) => {
             let link = document.createElement('link');
             link.href = styleSheet.href;
             link.rel = 'stylesheet';
+            link.onload = () => {
+                console.log('Stylesheet loaded:', styleSheet.href);
+            };
+            link.onerror = (error) => {
+                console.error('Error loading stylesheet:', styleSheet.href, error);
+            };
             doc.head.appendChild(link);
         } else if (styleSheet.cssRules) { // Inline stylesheet
             let style = document.createElement('style');
@@ -32,6 +67,7 @@ const DownloadInvoice = ({ rootElementId, downloadFileName }) => {
 
     // Clean up: remove the iframe after printing
     document.body.removeChild(iframe);
+
 
   };
   return (
